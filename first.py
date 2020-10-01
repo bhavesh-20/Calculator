a48c3cfd74f13d89068ea84e386976c8ccd96511
Navdeep.K <navdeep152002@gmail.com>
2020-10-01 15:03:17 +0530

    After merging

--- first.py
+++ first.py
@@ -32,6 +32,23 @@ sub = Button(root, text="-", command=general_click("-"))
 
 #row 2
 
+seven = Button(root, text="7", command=general_click("7"))
+eight = Button(root, text="8", command=general_click("8"))
+nine = Button(root, text="9", command=general_click("9"))
+add = Button(root, text="+", command=general_click("+"))
+
+#row 4
+
+one = Button(root, text="1", command=general_click("1"))
+two = Button(root, text="2", command=general_click("2"))
+three = Button(root, text="3", command=general_click("3"))
+
+#row 5
+
+zero = Button(root, text="0", command=general_click("0"))
+dot = Button(root, text=".", command=general_click("."))
+equ = Button(root, text="=", command=general_click("="))
+
 
 root = mainloop()
 
