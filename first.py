71ff186669b8d16937ab0cf6567ff8d95fff0479
Navdeep.K <navdeep152002@gmail.com>
2020-10-01 20:36:40 +0530

    Calculator with Frames

--- first.py
+++ first.py
@@ -5,12 +5,6 @@ root.title("Calculator")
 root.geometry("300x300")
 expression=StringVar()
 
-frame = Frame(root,highlightbackground="Black",width=300,height=50,highlightcolor="Black")
-frame.pack()
-
-bottom_frame = Frame(root,highlightbackground="Black",width=300,height=50,highlightcolor="Black")
-bottom_frame.pack()
-
 def general_click(item):
     global expression
     expression=expression+item
@@ -34,63 +28,76 @@ def equalto():
 expression=""
 display = StringVar()
 display.set(expression)
+
+frame = Frame(root,highlightbackground="Black",width=300,height=50,highlightcolor="Black")
+frame.pack()
 entry_field = Entry(frame,textvariable=display,width=300,bd=0,bg="Grey",fg="White",font=("arial",18,"bold"))
+
+#row 0
+
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+ac = Button(bottom_frame, text="AC", command=clear,bg="White",fg="Black",height=2,width=42)
+bottom_frame.pack()
 
 #row 1
 
-ac = Button(bottom_frame, text="AC", command=clear)
-div = Button(bottom_frame, text="/", command=partial(general_click,"/"))
-mul = Button(bottom_frame, text="*", command=partial(general_click,"*"))
-sub = Button(bottom_frame, text="-", command=partial(general_click,"-"))
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+div = Button(bottom_frame, text="/", command=partial(general_click,"/"),bg="White",fg="Black",height=2,width=10)
+mul = Button(bottom_frame, text="*", command=partial(general_click,"*"),bg="White",fg="Black",height=2,width=10)
+sub = Button(bottom_frame, text="-", command=partial(general_click,"-"),bg="White",fg="Black",height=2,width=10)
+add = Button(bottom_frame, text="+", command=partial(general_click,"+"),bg="White",fg="Black",height=2,width=10)
+bottom_frame.pack()
 
 #row 2
 
-seven = Button(bottom_frame, text="7", command=partial(general_click,"7"))
-eight = Button(bottom_frame, text="8", command=partial(general_click,"8"))
-nine = Button(bottom_frame, text="9", command=partial(general_click,"9"))
-add = Button(bottom_frame, text="+", command=partial(general_click,"+"))
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+seven = Button(bottom_frame, text="7", command=partial(general_click,"7"),bg="White",fg="Black",height=2,width=14)
+eight = Button(bottom_frame, text="8", command=partial(general_click,"8"),bg="White",fg="Black",height=2,width=14)
+nine = Button(bottom_frame, text="9", command=partial(general_click,"9"),bg="White",fg="Black",height=2,width=14)
+bottom_frame.pack()
 
 #row 3
-four = Button(bottom_frame,text="4",command=partial(general_click,"4"))
-five = Button(bottom_frame,text="5",command=partial(general_click,"5"))
-six = Button(bottom_frame,text="6",command=partial(general_click,"6"))
+
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+four = Button(bottom_frame,text="4",command=partial(general_click,"4"),bg="White",fg="Black",height=2,width=14)
+five = Button(bottom_frame,text="5",command=partial(general_click,"5"),bg="White",fg="Black",height=2,width=14)
+six = Button(bottom_frame,text="6",command=partial(general_click,"6"),bg="White",fg="Black",height=2,width=14)
+bottom_frame.pack()
 
 #row 4
 
-one = Button(bottom_frame, text="1", command=partial(general_click,"1"))
-two = Button(bottom_frame, text="2", command=partial(general_click,"2"))
-three = Button(bottom_frame, text="3", command=partial(general_click,"3"))
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+one = Button(bottom_frame, text="1", command=partial(general_click,"1"),bg="White",fg="Black",height=2,width=14)
+two = Button(bottom_frame, text="2", command=partial(general_click,"2"),bg="White",fg="Black",height=2,width=14)
+three = Button(bottom_frame, text="3", command=partial(general_click,"3"),bg="White",fg="Black",height=2,width=14)
+bottom_frame.pack()
 
 #row 5
 
-zero = Button(bottom_frame, text="0", command=partial(general_click,"0"))
-dot = Button(bottom_frame, text=".", command=partial(general_click,"."))
-equ = Button(bottom_frame, text="=", command=equalto)
+bottom_frame = Frame(root,highlightbackground="Black",width=300,height=250,highlightcolor="Black")
+zero = Button(bottom_frame, text="0", command=partial(general_click,"0"),bg="White",fg="Black",height=2,width=14)
+dot = Button(bottom_frame, text=".", command=partial(general_click,"."),bg="White",fg="Black",height=2,width=14)
+equ = Button(bottom_frame, text="=", command=equalto,bg="White",fg="Black",height=2,width=14)
+bottom_frame.pack()
 
-
-#entry_field.grid(column=0,row=0)
 entry_field.pack(ipady=10)
-"""
-ac.grid(column=0,row=0)
-div.grid(column=0,row=1)
-mul.grid(column=1,row=1)
-sub.grid(column=2,row=1)
-add.grid(column=3,row=1)
-seven.grid(column=0,row=2)
-eight.grid(column=1,row=2)
-nine.grid(column=2,row=2)
-four.grid(column=0,row=3)
-five.grid(column=1,row=3)
-six.grid(column=2,row=3)
-one.grid(column=0,row=4)
-two.grid(column=1,row=4)
-three.grid(column=2,row=4)
-zero.grid(column=0,row=5)
-dot.grid(column=1,row=5)
-equ.grid(column=2,row=5)
-"""
-ac.pack(side=RIGHT)
-
+ac.pack(side=LEFT)
+div.pack(side=LEFT)
+mul.pack(side=LEFT)
+sub.pack(side=LEFT)
+add.pack(side=LEFT)
+seven.pack(side=LEFT)
+eight.pack(side=LEFT)
+nine.pack(side=LEFT)
+four.pack(side=LEFT)
+five.pack(side=LEFT)
+six.pack(side=LEFT)
+one.pack(side=LEFT)
+two.pack(side=LEFT)
+three.pack(side=LEFT)
+zero.pack(side=LEFT)
+dot.pack(side=LEFT)
+equ.pack(side=LEFT)
 
 root = mainloop()
 
